71a862acdfd9581074e2b2821d82f173f4351392
Brandon Leger <51030112+Bran-Leger@users.noreply.github.com>
2021-11-03 16:10:33 -0400

    server.js 2.0

--- server.js
+++ server.js
@@ -9,16 +9,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(request, response){
   response.sendFile(__dirname + "/index.html");
+  // response.sendFile(__dirname + "/userAccountInfo.html");
 });
-
-app.get("/ay", function(req, res){
-  res.send("<h1>test<h1/>");
-});
-
-// app.get("/delItem", function(req, res){
-//   console.log(res.body);
-//   console.log(req.body);
-// })
 
 // login/register form has been submitted
 app.post("/", function(req, res){
@@ -37,12 +29,16 @@ app.post("/", function(req, res){
       // does the password match?
       if (userList.get(email) === password) {
         console.log("User/Password exist, logging in...");
+        console.log("serving up restaurant edit page");
+        res.redirect("/userAccountInfo");
       }
       else {
         console.log("User exists but incorrect password");
       }
     }
-    else console.log("User doesn't exist");
+    else {
+      console.log("User doesn't exist");
+    }
   }
   // did the user register?
   else if (Object.keys(req.body).includes("registerBtn")){
@@ -68,6 +64,21 @@ app.post("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/userAccountInfo", function(req, res){
+  res.sendFile(__dirname + "/userAccountInfo.html")
+});
+
+app.post("/userAccountInfo", function(req, res){
+  console.log(req.body);
+  let food = req.body["food"];
+  let cal = req.body["cal"];
+  let price = req.body["price"];
+
+
+  res.sendFile(__dirname + "/userAccountInfo.html")
+});
+
+
 // app.post("/delItem", function(req, res){
 //   console.log(req.body);
 // })
@@ -75,3 +86,59 @@ app.post("/", function(req, res){
 app.listen(3000, function(){
   console.log("server is running");
 });
+
+// Other Javascript
+function addTextField(x) {
+	x.style.display = "none";
+	x.parentNode.childNodes[7].style.display = "block";
+
+	let menuItem = x.parentNode.parentNode;
+
+	console.log(menuItem.childNodes[1].outerHTML);
+
+	for (let i = 0; i < menuItem.childNodes.length; i += 2) {
+		menuItem.childNodes[1].childNodes[i+1].outerHTML = "<input type = \"text\" value = \"" + menuItem.childNodes[1].childNodes[i+1].innerText + "\" name = \"test" + (i+1) + "\">";
+	}
+
+	console.log(menuItem.childNodes[1].outerHTML);
+}
+
+
+// DOES NOT WORK ENTIRELY
+function saveChanges(x) {
+	// Hide the Edit Item button and display the Save Changes button
+	x.style.display = "none";
+	x.parentNode.childNodes[1].style.display = "block";
+
+	// Define variables
+	let menuItem = x.parentNode.parentNode;
+	const names = [];
+
+	// Debugging output. Show the HTML for the menu object.
+	console.log(menuItem.outerHTML);
+
+	// Strip the item names from the text boxes and store them in names[]
+	for (let i = 1; i < 6; i += 2) {
+		let string = menuItem.childNodes[1].childNodes[i].outerHTML;
+		console.log(string);
+		let beginning = string.indexOf("value=") + 7;
+		let substr = string.substring(beginning, string.length);
+		let end = beginning + substr.indexOf("\"");
+		let name = string.substring(beginning, end);
+		console.log(name);
+		names[(i - 1)/2] = name;
+	}
+
+	// Overwrite the text boxes with the text they had contained. (Save the changes)
+	menuItem.childNodes[1].childNodes[1].outerHTML = "<div class = \"itemName\">" + names[0] + "</div>";
+  menuItem.childNodes[1].childNodes[3].outerHTML = "<div class = \"itemCalories\">" + names[1] + "</div>";
+	menuItem.childNodes[1].childNodes[5].outerHTML = "<div class = \"itemPrice\">" + names[2] + "</div>";
+
+	// Debugging output
+	//for (let i = 0; i < 6; i++) {
+	//	console.log(i + menuItem.childNodes[1].childNodes[i].outerHTML);
+	//}
+
+	// Debugging output. Show the HTML for the menu object.
+	console.log(menuItem.outerHTML);
+}
