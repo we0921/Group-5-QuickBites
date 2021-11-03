27d62272685429b138be65be4cc49ab496c8f9cf
DanielGomezWk <85709362+DanielGomezWk@users.noreply.github.com>
2021-11-03 02:37:07 -0400

    Update vendorEditMenu.js
    adding functionallity to add section button

--- vendorEditMenu.js
+++ vendorEditMenu.js
@@ -9,4 +9,138 @@ function addTextField(x) {
 	}   
 }
 
-function
+function addSection(button) {
+	let position = button.parentNode.parentNode;
+	
+	//adding <div class = "menuEditor">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "menuEditor";
+	
+	//adding <div class = "itemContainer">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "itemContainer";
+	
+	//--------------------------------------------top left menu item-------------------------------------------------------//
+	//adding <div class = "leftMenuItem">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "leftMenuItem";
+	
+	//adding <div class = "menuItemImage">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "menuItemImage";
+	position.innerHTML = "Food photo goes here";
+	
+	//adding <div class = "menuItemInfo">
+	position = position.parentNode;
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "menuItemInfo";
+	
+	//adding <div class = "itemInfo">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "itemInfo";
+	
+	//adding <div class = "itemButtonContainer">
+	position = position.parentNode;
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "itemButtonContainer";
+	
+	//--------------------------------------------top right menu item-------------------------------------------------------//
+	position = position.parentNode.parentNode.parentNode;
+	
+	//adding <div class = "rightMenuItem">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "rightMenuItem";
+	
+	//adding <div class = "menuItemImage">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "menuItemImage";
+	position.innerHTML = "Food photo goes here";
+	
+	//adding <div class = "menuItemInfo">
+	position = position.parentNode;
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "menuItemInfo";
+	
+	//adding <div class = "itemInfo">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "itemInfo";
+	
+	//adding <div class = "itemButtonContainer">
+	position = position.parentNode;
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "itemButtonContainer";
+	
+	//--------------------------------------------top left menu item-------------------------------------------------------//
+	
+	position = position.parentNode.parentNode.parentNode.parentNode;
+	//adding <div class = "leftMenuItem">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "leftMenuItem";
+	
+	//adding <div class = "menuItemImage">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "menuItemImage";
+	position.innerHTML = "Food photo goes here";
+	
+	//adding <div class = "menuItemInfo">
+	position = position.parentNode;
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "menuItemInfo";
+	
+	//adding <div class = "itemInfo">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "itemInfo";
+	
+	//adding <div class = "itemButtonContainer">
+	position = position.parentNode;
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "itemButtonContainer";
+	
+	//--------------------------------------------top right menu item-------------------------------------------------------//
+	position = position.parentNode.parentNode.parentNode;
+	
+	//adding <div class = "rightMenuItem">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "rightMenuItem";
+	
+	//adding <div class = "menuItemImage">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "menuItemImage";
+	position.innerHTML = "Food photo goes here";
+	
+	//adding <div class = "menuItemInfo">
+	position = position.parentNode;
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "menuItemInfo";
+	
+	//adding <div class = "itemInfo">
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "itemInfo";
+	
+	//adding <div class = "itemButtonContainer">
+	position = position.parentNode;
+	position.appendChild(document.createElement("div"));
+	position = position.lastChild;
+	position.className = "itemButtonContainer";
+}
