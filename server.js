8890f4d7f95aaeac83b940406ef17fa554242fc3
DanielGomezWk <85709362+DanielGomezWk@users.noreply.github.com>
2021-12-14 01:38:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -673,7 +673,7 @@ app.post("/adminCloseUserTicket", function(req, res){
 
 // added server port
 let port = process.env.PORT;
-if (port == null || port = "") {
+if (port == null || port == "") {
   port = 3000;
 }
 app.listen(port, function() {
